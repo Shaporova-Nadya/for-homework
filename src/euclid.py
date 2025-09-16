37d03ac46f0a92554eea4648222989c3980afa7d
Nadya Shaporova <saporovanada@gmail.com>
2025-09-16 13:10:31 +0300

    Add euclid algoritm homework

---
+++ src/euclid.py
@@ -0,0 +1,15 @@
+def gcd(a,b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        g, x1, y1 = gcd(b, a % b)
+        x = y1
+        y = x1 - y1 * (a//b)
+        return g, x, y
+
+a = int(input())
+b = int(input())
+g, x, y = gcd(a,b)
+print(f"НОД {a} и {b} равен {g}")
+print(f"x равен {x}, y равен {y}")
+print(f"Разложение Безу : {a}*{x} + {b}*{y} = {g}")

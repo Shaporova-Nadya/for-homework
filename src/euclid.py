089121568583bede48d03ba5f02b7f2ef2735cdf
Nadya Shaporova <saporovanada@gmail.com>
2025-12-07 23:54:09 +0300

    fixed this file

--- src/euclid.py
+++ src/euclid.py
@@ -7,11 +7,6 @@ def gcd(a,b):
         y = x1 - y1 * (a//b)
         return g, x, y
 
-a = int(input())
-b = int(input())
-g, x, y = gcd(a,b)
-print(f"НОД {a} и {b} равен {g}")
-print(f"x равен {x}, y равен {y}")
-print(f"Разложение Безу : {a}*{x} + {b}*{y} = {g}")
+
 
 

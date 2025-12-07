441b65f78a065f3cc722d4d1047f90276b3e2a05
Nadya Shaporova <saporovanada@gmail.com>
2025-12-08 02:10:51 +0300

    Changed tests

--- carr_hw/test_carr.py
+++ carr_hw/test_carr.py
@@ -6,20 +6,11 @@ def test_curry():
     result = curried_sum(1)(2)(3)
     assert result == 6
 
-    result_grouped = curried_sum(1, 2)(3)
-    assert result_grouped == 6
-    
-    result_mixed = curried_sum(1)(2, 3)
-    assert result_mixed == 6
-
 def test_curry_four():
     curried_mul = curry(multiply_four, 4)
     
     result = curried_mul(2)(3)(4)(5)
     assert result == 120 
-
-    result_grouped = curried_mul(2, 3)(4, 5)
-    assert result_grouped == 120
 
 def test_uncurry_sum():
     curried_sum = curry(sum_three_args, 3)
